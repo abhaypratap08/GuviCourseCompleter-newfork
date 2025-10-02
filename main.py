6e756573eb8b30cd9d2efe91ba2557b1d8ec89f7
M B SURYANARAYANAN <mbsurya19122004@gmail.com>
2025-10-02 12:23:11 +0530

    given up

--- main.py
+++ main.py
@@ -17,27 +17,44 @@ def installExtension():
     time.sleep(3)
 
 def gotoCourse():
+    current_lecture = 0 
+    gotLecture = False
     driver.get('https://www.guvi.in/courses-video/?course=galgotiasservletsandjsp')
-    video = driver.find_element(By.CSS_SELECTOR, ".plyr__control")
-    video.click()
-    key.keyDown('D') 
-    time.sleep(3)           
-    key.keyUp('D') 
+    checkDone = driver.find_element(By.XPATH, '/html/body/main/div[6]/div/div[3]/div[3]/div[1]/div/ul/li[1]/a')
+    while gotLecture==False:
+        if checkDone.get_attribute("disabled")=="disabled":
+            print("Course not completed")
+            gotLecture = True
+            videoPlay = driver.find_element(By.CSS_SELECTOR, ".plyr__control")
+            videoPlay.click()
+            count=0
+            while count<150:
+                key.press('d')
+                count+=1
+        else:
+            print("Course Completed")
+            listCourse = driver.find_elements(By.XPATH, "/html/body/main/div[6]/div/div[3]/div[4]/div/div[2]/div[1]/div[2]/div/ul")
+            print(listCourse)
+            current_lecture+=1
+            listCourse[current_lecture].click()
 
-
+def checkVideo():
+    timeVid = driver.find_element(By.XPATH, '/html/body/main/div[6]/div/div[1]/div[1]/div[1]/div[1]/div[2]')
     
-
-
+    while timeVid.text!="00:00":
+        print(timeVid.text)
+        time.sleep(2)
 
 if __name__=="__main__":
     driver = webdriver.Chrome()  
     driver.get("https://www.guvi.in/sign-in/") 
-    driver.maximize_window()
 
+    
     Login()
     time.sleep(1)
     installExtension()
     gotoCourse()
+    checkVideo()
   
 
     time.sleep(10)
